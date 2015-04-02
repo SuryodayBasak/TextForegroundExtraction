361cded989e5826df60c579f700751bac2b58f07
Suryoday Basak <suryodaybasak@gmail.com>
2015-04-02 16:47:01 +0530

    Found matrix of all white pixels on black pixels

--- src/skew_correction.py
+++ src/skew_correction.py
@@ -1,3 +1,27 @@
 #python code to correct skew angle in images
 
 #need to use concept of Eigen Vectors here
+
+import cv2
+import binarization
+
+img = cv2.imread('/home/suryo/Image_Processing_Exercises/IISC/resources/Kandanu10.jpg',0)
+binary = binarization.binary_img(img)
+cv2.imshow('binary', binary)
+
+height, width = img.shape[:2]
+
+matrix = []
+
+print height
+print width
+
+for i in range(0,height):
+    for j in range(0,width):
+        if(binary.item(i,j)==255):
+            matrix.append((i,j))
+            
+print matrix
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
