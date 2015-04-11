760e8d5065f7259a3b0c054ab95669b409132945
Suryoday Basak <suryodaybasak@gmail.com>
2015-04-11 20:50:31 +0530

    Getting somewhere with skew correction...

--- src/preproc_for_skew_correction_2.py
+++ src/preproc_for_skew_correction_2.py
@@ -25,8 +25,8 @@ cnt=contours[max_index]
 
 print max_index
 
-binary = cv2.drawContours(binary, contours, max_index-1, (0,255,0), 3)
-cv2.imshow('largest', binary)
+cv2.drawContours(mask, contours, max_index-1, (0,255,0), 3)
+cv2.imshow('largest', mask)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
