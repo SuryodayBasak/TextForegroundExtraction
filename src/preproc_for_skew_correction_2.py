f283fb95f899700e2b6830bb07a003edf63bdda9
Suryoday Basak <suryodaybasak@gmail.com>
2015-04-11 19:20:50 +0530

    Preprocessing for skew correction

--- src/preproc_for_skew_correction_2.py
+++ src/preproc_for_skew_correction_2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr  9 23:20:03 2015
+Created on Sat Apr 11 19:08:52 2015
 
 @author: suryo
 """
@@ -12,27 +12,21 @@ kernel = np.ones((5,5),np.uint8)
 
 img = cv2.imread('/home/suryo/Image_Processing_Exercises/IISC/resources/Kandanu10.jpg',0)
 cv2.imshow('original', img)
+mask = np.zeros(img.shape[:2],np.uint8)
+
 binary = binarization.binary_img(img)
 cv2.imshow('binary', binary)
 
-dil = cv2.dilate(binary,kernel,iterations = 1)
-cv2.imshow('dil', dil)
-
+#contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-# Find the index of the largest contour
 areas = [cv2.contourArea(c) for c in contours]
 max_index = np.argmax(areas)
 cnt=contours[max_index]
 
 print max_index
-print cnt
 
-x,y,w,h = cv2.boundingRect(cnt)
-cv2.rectangle(dil,(x,y),(x+w,y+h),(0,255,0),-1)
-
-cv2.imshow('im', dil)
-
+binary = cv2.drawContours(binary, contours, max_index-1, (0,255,0), 3)
+cv2.imshow('largest', binary)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
