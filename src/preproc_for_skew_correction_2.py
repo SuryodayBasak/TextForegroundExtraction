a9fbf5f198e9fe5829ba2dbe9ce7ec4b3aa81506
Suryoday Basak <suryodaybasak@gmail.com>
2015-04-11 21:07:08 +0530

    Skew corrected

--- src/preproc_for_skew_correction_2.py
+++ src/preproc_for_skew_correction_2.py
@@ -81,13 +81,13 @@ angle = (np.arctan2(y,x))*(180/np.pi)
 print angle
 
 #M = cv2.getRotationMatrix2D((width/2,height/2),-angle/2,1)
-M = cv2.getRotationMatrix2D((width/2,height/2),-angle,1)
-dst = cv2.warpAffine(binary,M,(width,height))
+M = cv2.getRotationMatrix2D((width/2,height/2),-(90+angle),1)
+dst = cv2.warpAffine(img,M,(width,height))
 
 print type(dst)
 
 cv2.imshow('dst', dst)
-#cv2.imwrite('skewcorrected.jpg',dst)
+cv2.imwrite('skewcorrected.jpg',dst)
 
 
 
