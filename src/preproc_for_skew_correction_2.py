f51ac4ddb3f3cca84acd2ebd3487f4fa5472fa31
Suryoday Basak <suryodaybasak@gmail.com>
2015-04-11 20:57:05 +0530

    Getting somewhere with skew correction...

--- src/preproc_for_skew_correction_2.py
+++ src/preproc_for_skew_correction_2.py
@@ -25,9 +25,11 @@ cnt=contours[max_index]
 
 print max_index
 
-cv2.drawContours(mask, contours, max_index, (255,255,255), 3)
+cv2.drawContours(mask, contours, max_index, (255,255,255), 2)
 cv2.imshow('largest', mask)
-cv2.imwrite('largest.jpg', mask)
+#cv2.imwrite('largest.jpg', mask)
+
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
