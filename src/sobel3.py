509c95d34f3f5e34e594d6e57bc2d1efd1864c33
Suryoday Basak <suryodaybasak@gmail.com>
2015-04-22 21:15:49 +0530

    smoothening images in sobel3.py

--- src/sobel3.py
+++ src/sobel3.py
@@ -12,6 +12,7 @@ import binarization
 kernel = np.ones((5,5),np.uint8)
 
 img = cv2.imread('/home/suryo/Image_Processing_Exercises/IISC/resources/Kandanu10.jpg',0)
+img=cv2.medianBlur(img,5)
 largest_contour = np.zeros(img.shape[:2],np.uint8)
 
 sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
@@ -22,10 +23,6 @@ sobel_8u = np.uint8(abs_sobel64f)
 #sobely = cv2.erode(sobely,kernel,iterations = 1)
 
 cv2.imshow('sobely',sobel_8u)
-thresh = 1
-bina = cv2.threshold(sobel_8u, thresh, 255, cv2.THRESH_BINARY)[1]
-cv2.imshow('bina',bina)
-
 
 #contours, hierarchy = cv2.findContours(sobely,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
