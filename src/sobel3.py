6dda9f513b73f71a9c2a1fff5a105de163ef7b0f
Suryoday Basak <suryodaybasak@gmail.com>
2015-04-22 20:51:29 +0530

    adding sobel3.oy

---
+++ src/sobel3.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 20:41:19 2015
+
+@author: suryo
+"""
+
+import cv2
+import numpy as np
+import binarization
+
+kernel = np.ones((5,5),np.uint8)
+
+img = cv2.imread('/home/suryo/Image_Processing_Exercises/IISC/resources/Kandanu10.jpg',0)
+largest_contour = np.zeros(img.shape[:2],np.uint8)
+
+sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+sobely = cv2.erode(sobely,kernel,iterations = 1)
+cv2.imshow('sobely',sobely)
+
+#contours, hierarchy = cv2.findContours(sobely,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
